c49908ccf115fb4af7d2a23b4d3bd30913c25d81
suzuki-hina <ichigodaifuku.0530@gmail.com>
2021-05-14 14:08:31 +0900

    完成

--- docs/script.js
+++ docs/script.js
@@ -11,6 +11,16 @@ const renderJson = (json) => {
    const studioDiv = document.createElement('div');
    studioDiv.className ='studio-div';
 
+
+   const StudioTitleJa = document.createElement("h2");
+   StudioTitleJa.className = 'studio-title-ja';
+   StudioTitleJa.textContent = studio['name-ja'];
+
+   const StudioTitleEn = document.createElement("h2");
+   StudioTitleEn.className = 'studio-title-en';
+   StudioTitleEn.textContent = studio['name-en'];
+
+
    const ContentsDiv = document.createElement('div');
    ContentsDiv.className ='contents';
 
@@ -29,21 +39,25 @@ const renderJson = (json) => {
    const ContentsItem2Div = document.createElement('div');
    ContentsItem2Div.className ='contents_item2';
 
-   const StudioTitleJa = document.createElement("h2");
-   StudioTitleJa.className = 'studio-title-ja';
-   StudioTitleJa.textContent = studio['name-ja'];
-
-   const StudioTitleEn = document.createElement("h3");
-   StudioTitleEn.className = 'studio-title-en';
-   StudioTitleEn.textContent = studio['name-en'];
-
-   const StudioCoreJa = document.createElement("h4");
+   const StudioCoreJa = document.createElement("h3");
    StudioCoreJa.className = 'studio-core-ja';
    StudioCoreJa.textContent = studio['core-ja'];
 
-   const StudioCoreEn = document.createElement("h4");
+   const StudioCoreEn = document.createElement("h3");
    StudioCoreEn.className = 'studio-core-en';
    StudioCoreEn.textContent = studio['core-en'];
+
+   const StudioTan = document.createElement("h4");
+   StudioTan.className = 'studio-tan';
+   StudioTan.textContent = '担当教員';
+
+   const StudioFacultyJa = document.createElement("span");
+   StudioFacultyJa .className = 'studio-faculty-ja';
+   StudioFacultyJa .textContent = studio['faculty-ja'];
+
+   const StudioFacultyTitleJa = document.createElement("span");
+   StudioFacultyTitleJa.className = 'studio-faculty-title-ja';
+   StudioFacultyTitleJa.textContent = studio['faculty-title-ja'];
 
 
 
@@ -57,17 +71,22 @@ const renderJson = (json) => {
 
    ContentsItem1Div.appendChild(studioImage);
 
-   ContentsItem2Div.appendChild(StudioTitleJa);
-   ContentsItem2Div.appendChild(StudioTitleEn);
+   
    ContentsItem2Div.appendChild(StudioCoreJa);
    ContentsItem2Div.appendChild(StudioCoreEn);
+   ContentsItem2Div.appendChild(StudioTan);
+   ContentsItem2Div.appendChild(StudioFacultyJa);
+   ContentsItem2Div.appendChild(StudioFacultyTitleJa);;
 
    ContentsItem3Div.appendChild(StudioDesJa);
 
+   
    ContentsDiv.appendChild(ContentsItem1Div);
    ContentsDiv.appendChild(ContentsItem2Div);
    ContentsDiv.appendChild(ContentsItem3Div);
 
+   studioDiv.appendChild(StudioTitleJa);
+   studioDiv.appendChild(StudioTitleEn);
    studioDiv.appendChild(ContentsDiv);
   
    document.getElementById('studios').appendChild(studioDiv);
